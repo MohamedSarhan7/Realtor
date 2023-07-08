f334bcd8aa584b288e181b23120d68b9cec23157
Mohamed Sarhan <mohsarhan88@gmail.com>
2023-07-08 22:10:43 +0300

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
@@ -15,6 +17,18 @@ async function bootstrap() {
   );
   app.enableVersioning();
   app.enableCors({ origin: '*' });
+
+  const config = new DocumentBuilder()
+    .setTitle('Reaeltor App')
+    .setDescription('The Reaeltor App API description')
+    .setVersion('1.0')
+    .addTag('Reaeltor App')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+  logger.log('Swagger docs at http://localhost:3000/api/docs');
   await app.listen(3000);
+  logger.log('app listening on port 3000');
 }
 bootstrap();
